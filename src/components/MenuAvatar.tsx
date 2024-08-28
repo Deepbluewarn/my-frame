307a35eb4a1efa571c55bf1633a11dd53f263c71
bluewarn <n5lp97@gmail.com>
2024-08-28 10:33:01 +0900

    dropdown 메뉴를 포함한 avatar 컴포넌트 추가

---
+++ src/components/MenuAvatar.tsx
@@ -0,0 +1,43 @@
+import { getSession } from '@auth0/nextjs-auth0';
+import { Avatar, Button, Menu, MenuDivider, MenuDropdown, MenuItem, MenuLabel, MenuTarget, Text, rem } from '@mantine/core';
+import {
+    IconSearch,
+    IconPhoto,
+    IconLogout,
+} from '@tabler/icons-react';
+
+export default async function MenuAvatar() {
+    const session = await getSession();
+    const user = session?.user;
+
+    if (!user) {
+        return null
+    }
+
+    return (
+        <Menu shadow="md" width={200}>
+            <MenuTarget>
+                <Avatar src={user.picture} alt={user.name} />
+            </MenuTarget>
+
+            <MenuDropdown>
+                <MenuItem leftSection={<IconPhoto style={{ width: rem(14), height: rem(14) }} />}>
+                    내 사진
+                </MenuItem>
+                <MenuItem
+                    leftSection={<IconSearch style={{ width: rem(14), height: rem(14) }} />}
+                >
+                    검색
+                </MenuItem>
+
+                <MenuDivider />
+                <MenuItem
+                    color="red"
+                    leftSection={<IconLogout style={{ width: rem(14), height: rem(14) }} />}
+                >
+                    <a href='/api/auth/logout'>로그아웃</a>
+                </MenuItem>
+            </MenuDropdown>
+        </Menu>
+    );
+}
