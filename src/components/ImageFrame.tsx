e50325801d52354cf2b36ab0d11cfcc6ba636184
bluewarn <n5lp97@gmail.com>
2024-08-31 14:35:51 +0900

    ImageFrame 컴포넌트를 별도의 파일로 분리

---
+++ src/components/ImageFrame.tsx
@@ -0,0 +1,25 @@
+import { UploadContext } from '@/context/UploadContext';
+import { ImageInterface } from '@/interface/Upload';
+import Styles from '@/styles/components/ImageFrame.module.css'
+import { Paper } from '@mantine/core';
+import { useContext } from 'react';
+
+export default function ImageFrame({ image } : { image: ImageInterface }) {
+    const uploadContext = useContext(UploadContext);
+
+    const handleImageClick = (image_key: string) => {
+        uploadContext.setSelectedFileKey(image_key);
+    }
+
+    return (
+        <Paper shadow="xs" className={Styles.container}>
+            <img
+                key={image.key}
+                src={URL.createObjectURL(image.object)}
+                alt={image.name}
+                className={Styles.image}
+                onClick={() => handleImageClick(image.key)}
+            />
+        </Paper>
+    )
+}
--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -3,30 +3,9 @@
 import { UploadAction } from "@/actions/upload/upload";
 import { UploadContext } from "@/context/UploadContext";
 import React, { useContext } from "react";
-import { Paper } from '@mantine/core'
 import { ImageInterface } from "@/interface/Upload";
 import { hashFile, readFile } from "@/utils/file";
-import Styles from '@/styles/components/ImageFrame.module.css'
-
-function ImageFrame({ image } : { image: ImageInterface }) {
-    const uploadContext = useContext(UploadContext);
-
-    const handleImageClick = (image_key: string) => {
-        uploadContext.setSelectedFileKey(image_key);
-    }
-
-    return (
-        <Paper shadow="xs" className={Styles.container}>
-            <img
-                key={image.key}
-                src={URL.createObjectURL(image.object)}
-                alt={image.name}
-                className={Styles.image}
-                onClick={() => handleImageClick(image.key)}
-            />
-        </Paper>
-    )
-}
+import ImageFrame from "./ImageFrame";
 
 export default function Uploader() {
     const uploadContext = useContext(UploadContext);
