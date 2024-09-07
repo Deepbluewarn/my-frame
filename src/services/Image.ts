6681e03cf652fb1d7f93fd33d33e82e6e0d72313
bluewarn <n5lp97@gmail.com>
2024-09-07 18:52:24 +0900

    이미지 서비스 함수 추가

--- src/services/Image.ts
+++ src/services/Image.ts
@@ -3,3 +3,15 @@ import Image, { ImageInterface } from "@/db/models/Image";
 export async function createImage(image: ImageInterface) {
     return (await new Image(image)).save();
 }
+
+export async function getAllImagesByOwner(owner: string) {
+    return (await Image.find({ owner }));
+}
+
+export async function getRecentImagesByOwner(owner: string, limit: number) {
+    return (await Image.find({ owner }).sort({ createdAt: -1 }).limit(limit));
+}
+
+export async function getRecentPublicImages(limit: number) {
+    return (await Image.find({ visibility: 'public' }).sort({ createdAt: -1 }).limit(limit));
+}
