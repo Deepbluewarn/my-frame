9950e859a31f54b34db1938d87928111a40abd9c
bluewarn <n5lp97@gmail.com>
2024-09-07 15:47:17 +0900

    refector: Auth0 Actions로 유저 정보를 DB에 저장하고 실패 시 로그인을 취소.
    로그인 또는 회원 가입 -> onExecutePostLogin에서 Landing 페이지 요청하여 DB 작업 수행 -> onContinuePostLogin에서 DB 작업 성공 여부 확인하고 실패 시 /auth/error 페이지로 redirect.

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,33 +1,9 @@
-// app/api/auth/[auth0]/route.js
-import { handleAuth, handleCallback, Session } from '@auth0/nextjs-auth0';
-import { NextRequest } from 'next/server';
-import connectDB from '@/db/init';
-import { UserInterface } from '@/db/models/User';
-import { createUser, getUserBySub, updateUserBySub } from '@/services/User';
-
-const afterCallback = async (req: NextRequest, session: Session) => {
-    await connectDB();
-
-    const sub = session.user.sub;
-    const userData: Partial<UserInterface> = {
-        sub,
-        username: session.user.nickname,
-        email: session.user.email,
-        profilePicture: session.user.picture,
-    };
-
-    const existingUser = await getUserBySub(sub);
-
-    if (!existingUser) {
-        createUser(userData);
-    } else {
-        userData.updatedAt = new Date();
-        updateUserBySub(sub, userData);
-    }
-    
-    return session;
-};
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
-    callback: handleCallback({ afterCallback })
+    login: handleLogin({
+        authorizationParams: {
+            prompt: 'login',
+        }
+    })
 });
---
+++ src/app/landing/page.tsx
@@ -0,0 +1,47 @@
+import { UserInterface } from "@/db/models/User";
+import { createUser, getUserBySub, updateUserBySub } from "@/services/User";
+import { decodeJwt } from "@/utils/jwt";
+import { JwtPayload } from "jsonwebtoken";
+import { redirect } from "next/navigation";
+
+function redirectToContinue(state: string, success: boolean) {
+    redirect(`${process.env.AUTH0_ISSUER_BASE_URL}/continue?state=${state}&dbSuccess=${success}`);
+}
+export default async function Landing({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined }
+}) {
+    const state = searchParams.state;
+    const token = searchParams.session_token;
+    let success = false;
+
+    const payload = decodeJwt(token as string) as JwtPayload;
+
+    if (!payload) redirectToContinue(state as string, success);
+
+    const sub = payload!.user_id;
+    const username = payload!.nickname || payload!.name;
+    const email = payload!.email;
+    const profilePicture = payload!.picture;
+
+    const userData: Partial<UserInterface> = {
+        sub, username, email, profilePicture,
+    };
+
+    try {
+        const existingUser = await getUserBySub(sub);
+
+        if (!existingUser) {
+            await createUser(userData);
+        } else {
+            userData.updatedAt = new Date();
+            await updateUserBySub(sub, userData);
+        }
+        success = true;
+    } catch (e) {
+        success = false;
+    } finally {
+        redirectToContinue(state as string, success);
+    }
+}
