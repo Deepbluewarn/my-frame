21f81b89d387115317310d0e96aeb7dad3750940
bluewarn <n5lp97@gmail.com>
2024-09-27 10:46:23 +0900

    인증이 필요한 경로 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,9 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: '/upload'
+  matcher: [
+    '/upload',
+    '/image', 
+    '/user'
+  ]
 };
