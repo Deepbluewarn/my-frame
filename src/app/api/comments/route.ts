49dd9ef535189efc9f75e1c79f4ed50ed058a0d8
bluewarn <n5lp97@gmail.com>
2025-02-07 05:53:38 +0000

    타입 오류 수정

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
     let res: IComment[] = [];
 
     if (!imageId) {
-        return res;
+        return Response.json(res);
     }
     res = await getImageComments(imageId);
 
