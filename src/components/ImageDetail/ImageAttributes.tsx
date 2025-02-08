226f84ed219f62f7aead51422c71f5070d593e0b
bluewarn <n5lp97@gmail.com>
2025-02-08 06:38:34 +0000

    fix: uploadedAt 속성을 Date 객체로 변환하여 직렬화 문제 해결
    이미지 목록을 가져오는 함수를 action에서 fetch로 바꾸면서 객체 직렬화가 발생하여 Date 객체가 문자열로 바뀌면서 오류가 발생함.

--- src/components/ImageDetail/ImageAttributes.tsx
+++ src/components/ImageDetail/ImageAttributes.tsx
@@ -11,12 +11,17 @@ export default function ImageAttributes() {
     return (
         <List>
             <Text fw={700}>이미지 정보</Text>
+            {
+                process.env.NODE_ENV === 'development' ? (
+                    <List.Item>_id: {currentImage._id}</List.Item>
+                ) : null
+            }
             <List.Item>너비: {currentImage.width}px</List.Item>
             <List.Item>높이: {currentImage.height}px</List.Item>
             <List.Item>업로드 날짜: {
                 new Intl.DateTimeFormat('ko-KR',
                     { timeZone: 'Asia/Seoul', dateStyle: 'full', timeStyle: 'short' }
-                ).format(currentImage.uploadedAt)
+                ).format(new Date(currentImage.uploadedAt))
             }
             </List.Item>
         </List>
